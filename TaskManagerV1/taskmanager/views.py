72053b7160a86842101c37af9bfe612a104fe8d3
dnCRevelation <jp.wilkinson@hotmail.com>
2024-10-10 22:07:44 -0400

    App polished and completed.

---
+++ TaskManagerV1/taskmanager/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Task
+
+def home(request):
+    tasks = Task.objects.all().order_by('-id')
+    return render(request, 'index.html', {
+        'tasks': tasks
+    })
+
+def completed(request):
+    completed_task = Task.objects.filter(completed=True)
+
+    return render(request, 'completed.html', {
+        'tasks': completed_task,
+    })
+
+def remaining(request):
+    remaining_task = Task.objects.filter(completed=False)
+
+    return render(request, 'remaining.html', {
+        'tasks': remaining_task,
+    })
+
+def add_task(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        due_date = request.POST.get('due_date')
+        due_time = request.POST.get('due_time')
+        completed = False
+
+        if title != "" and due_date != "" and due_time != "":
+            task = Task(
+                title=title,
+                description=description,
+                due_date=due_date,
+                due_time=due_time,
+                completed=completed
+            )
+            task.save()
+            return redirect('home')
+    else:
+        return render(request, 'add_task.html')
+    return render(request, 'add_task.html')
+
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+
+    return render(request, 'delete.html', {
+        "task": task
+    })
+
+def task_detail(request, task_id):
+    task = Task.objects.get(id=task_id)
+    return render(request, 'task_detail.html', {
+        "task": task,
+    })
+
+def toggle_complete(request, task_id):
+    task = Task.objects.get(id=task_id)
+    if task:
+        task.completed = not task.completed
+        task.save()
+        return redirect('home')
+    
+def remove_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    if task:
+        task.delete()
+        return redirect('home')
